14a5847b0b123d479496a4bc29a929655ec1e2c4
Restu-Averian <restu.putra@dataon.com>
2025-05-24 00:58:08 +0700

    feat: add animation for logo while route is /

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, Flex, HStack, Image } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import navbar from "../constants/navbar";
 import isActive from "../helpers/isActive";
 import "../style/button.css";
@@ -7,12 +7,40 @@ import "../style/button.css";
 const Navbar = () => {
   const navigate = useNavigate();
 
+  const { pathname } = useLocation();
+
+  const isHome = pathname === "/";
+
   return (
     <Flex justifyContent="space-between" py="30px">
-      <Box onClick={() => navigate("/")} cursor="pointer">
-        <Image src="logo.png" width={12} />
+      <Box
+        onClick={() => navigate("/")}
+        cursor="pointer"
+        position="relative"
+        transition="all 300ms"
+        {...(isHome
+          ? {
+              top: "7rem",
+              left: "50%",
+              transform: "translateX(-50%)",
+            }
+          : {
+              top: 0,
+              left: 10,
+            })}
+      >
+        <Image
+          src="logo.png"
+          width={isHome ? 120 : 12}
+          transition="all 300ms"
+        />
       </Box>
-      <Box>
+
+      <Box
+        position="relative"
+        right={isHome ? "35%" : 0}
+        transition="all 300ms"
+      >
         <HStack spacing="50px">
           {navbar?.map((n, idx) => (
             <Button
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,4 @@
-import {
-  AbsoluteCenter,
-  Box,
-  Heading,
-  SimpleGrid,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 import Icon from "../components/Icon";
 import contacts from "../constants/contacts";
 import Helmet from "../components/Helmet";
@@ -15,41 +8,41 @@ const Home = () => {
   const { xs } = useResponsive();
   return (
     <Helmet title="Home">
-      <Box position="relative" h="90vh">
-        <AbsoluteCenter axis="both" w="100%" textAlign="center">
-          <Stack spacing={50}>
-            <Stack>
-              <Box>
-                <Heading fontSize={xs ? "50px" : "100px"} as="h1">
-                  Restu Averian Putra
-                </Heading>
-              </Box>
-              <Box>
-                <Text fontSize={xs ? "24px" : "32px"}>
-                  Aspiring to become a proficient Front-End Developer
-                </Text>
-              </Box>
-            </Stack>
-
-            <SimpleGrid
-              columns={xs ? 2 : 4}
-              justifyContent="center"
-              margin="auto"
-              spacing={xs ? 50 : 100}
+      <Stack spacing={50} position="relative" pt="5rem">
+        <Stack>
+          <Box>
+            <Heading
+              fontSize={xs ? "50px" : "100px"}
+              as="h1"
+              textAlign="center"
             >
-              {contacts?.map((c, idx) => (
-                <Box
-                  key={idx}
-                  onClick={() => window.open(c?.path, "_blank")}
-                  cursor="pointer"
-                >
-                  <Icon name={c?.iconName} />
-                </Box>
-              ))}
-            </SimpleGrid>
-          </Stack>
-        </AbsoluteCenter>
-      </Box>
+              Restu Averian Putra
+            </Heading>
+          </Box>
+          <Box>
+            <Text fontSize={xs ? "24px" : "32px"} textAlign="center">
+              Aspiring to become a proficient Front-End Developer
+            </Text>
+          </Box>
+        </Stack>
+
+        <SimpleGrid
+          columns={xs ? 2 : 4}
+          justifyContent="center"
+          margin="auto"
+          spacing={xs ? 50 : 100}
+        >
+          {contacts?.map((c, idx) => (
+            <Box
+              key={idx}
+              onClick={() => window.open(c?.path, "_blank")}
+              cursor="pointer"
+            >
+              <Icon name={c?.iconName} />
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Stack>
     </Helmet>
   );
 };
